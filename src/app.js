8fe5523a13b78c1e7fa066b3af6ac7738721b2b5
bvndls <controleverything64@gmail.com>
2023-09-26 16:49:14 +0200

    Moved to TailwindCSS, refactored for adaptive UI

---
+++ src/app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // generate random colors
+  function getRandomColor() {
+    const colors = ['#D5EFFD', '#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  // define elements from stylesheet
+  const spanElement = document.querySelector('span.stubtext');
+  const darkModeIcon = document.querySelector('.darkmode');
+
+  // initial text color
+  // spanElement.style.color = '##010d13';
+
+  // animation
+  setInterval(function() {
+    spanElement.style.transition = 'color 5s';
+    spanElement.style.color = getRandomColor();
+  }, 2000);
+
+// click listener
+darkModeIcon.addEventListener('click', function() {
+  document.documentElement.classList.toggle('dark');
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'bgcolor': '#ecf8fe',
+      'txtcolor': '#010d13',
+      'primary': '#0b8fd0',
+      'secondary': '#d5effd',
+      'accent': '#054667',
+    },
+    extend: {
+      fontFamily :{
+        alexandria: ["alexandria","sans-serif"],
+        omotion: ["omotion","sans"],
+      }
+    },
+  },
+  plugins: [],
+}
