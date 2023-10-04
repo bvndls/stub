ab96e6f6dc13a27e1a895402c058a38e61a5dfdc
bvndls <controleverything64@gmail.com>
2023-10-04 02:44:30 +0200

    arcfeature styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       'primary': '#0b8fd0',
       'secondary': '#d5effd',
       'accent': '#054667',
+      'dark_bgcolor': '#010d13',
+      'dark_txtcolor': '#ecf8fe',
+      'dark_primary': '#2fb2f4',
+      'dark_secondary': '#021d2c',
+      'dark_accent': '#99dafa'
     },
     extend: {
       fontFamily: {
