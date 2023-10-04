506e304574942bf1328b44c93e31e3abedef65ca
bvndls <controleverything64@gmail.com>
2023-10-04 03:31:09 +0200

    arcfeature color grab

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,27 @@ module.exports = {
       'dark_txtcolor': '#ecf8fe',
       'dark_primary': '#2fb2f4',
       'dark_secondary': '#021d2c',
-      'dark_accent': '#99dafa'
+      'dark_accent': '#99dafa',
+      // arc colors section
+      'foregroundPrimary': 'var(--arc-palette-foregroundPrimary)',
+      'foregroundSecondary': 'var(--arc-palette-foregroundSecondary)',
+      'foregroundTertiary': 'var(--arc-palette-foregroundTertiary)',
+      'maxContrast': 'var(--arc-palette-maxContrastColor)',
+      'minContrast': 'var(--arc-palette-minContrastColor)',
+      'backgroundExtra': 'var(--arc-palette-backgroundExtra)',
+      'background': 'var(--arc-palette-background)',
+      'focus': 'var(--arc-palette-focus)',
+      'hover': 'var(--arc-palette-hover)',
+      'cutout': 'var(--arc-palette-cutoutColor)',
+      'title': 'var(--arc-palette-title)',
+      'subtitle': 'var(--arc-palette-subtitle)',
+      'background-simple': 'var(--arc-background-simple-color)',
+      'background-gradient0': 'var(--arc-background-gradient-color0)',
+      'background-gradient1': 'var(--arc-background-gradient-color1)',
+      'background-gradient2': 'var(--arc-background-gradient-color2)',
+      'background-gradient-overlay0': 'var(--arc-background-gradient-overlay-color0)',
+      'background-gradient-overlay1': 'var(--arc-background-gradient-overlay-color1)',
+      'background-gradient-overlay2': 'var(--arc-background-gradient-overlay-color2)',
     },
     extend: {
       fontFamily: {
