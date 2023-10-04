631ca8e3268c9e6621ddfe9d8ddf18fc468f000c
bvndls <controleverything64@gmail.com>
2023-10-04 18:38:53 +0200

    arcfeature logic update

--- src/app.js
+++ src/app.js
@@ -10,12 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
   const darkModeIcon = document.querySelector('.darkmode');
   const arcFeatureDiv = document.getElementById("arcfeature");
   
+  // variables to track original classes
+  let wasDark = false;
+  let wasArc = false;
+
   // arcfeature click listener
   arcFeatureDiv.addEventListener("click", function() {
-    if(document.documentElement.classList.contains('dark')) {
-      document.documentElement.classList.remove('dark');
+    if (document.documentElement.classList.contains("arc")) {
+      document.documentElement.classList.remove("arc");
+      if (wasDark) {
+        document.documentElement.classList.add("dark");
+        wasDark = false;
+      }
+    } else {
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        wasDark = true;
+      }
+      document.documentElement.classList.add("arc");
     }
-    document.documentElement.classList.toggle("arc");  
   });
 
   // animation
@@ -26,10 +39,19 @@ document.addEventListener('DOMContentLoaded', function() {
   
   // darkmode click listener
   darkModeIcon.addEventListener('click', function() {
-    if(document.documentElement.classList.contains("arc")) {
-      document.documentElement.classList.remove("arc");
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      if (wasArc) {
+        document.documentElement.classList.add("arc");
+        wasArc = false;
+      }
+    } else {
+      if (document.documentElement.classList.contains("arc")) {
+        document.documentElement.classList.remove("arc");
+        wasArc = true;
+      }
+      document.documentElement.classList.add("dark");
     }
-    document.documentElement.classList.toggle('dark');
   });
 });
 
